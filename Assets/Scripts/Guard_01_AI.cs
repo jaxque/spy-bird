043969bf0829604c1215ec2d2b7291b29242c622
kylemarz <80406277+kylemarz@users.noreply.github.com>
2021-03-23 14:57:43 -0400

    navmesh and play movement

---
+++ Assets/Scripts/Guard_01_AI.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Guard_01_AI : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    int currentWaypoint = 0;
+
+    enum GuardState {Patrol, Pursue};
+    GuardState currentState = GuardState.Patrol;
+
+    NavMeshAgent navMA;
+
+    GameObject playerTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMA = GetComponent<NavMeshAgent>();
+        navMA.SetDestination(waypoints[currentWaypoint].position);
+    }
+
+    void SwitchToState(GuardState newState)
+    {
+        switch (newState)
+        {
+            case GuardState.Patrol:
+                break;
+            case GuardState.Pursue:
+                navMA.SetDestination(playerTarget.transform.position);
+                break;
+            default:
+                break;
+        }
+        currentState = newState;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case GuardState.Patrol:
+                if (navMA.remainingDistance < 0.04f)
+                {
+                    currentWaypoint = (currentWaypoint +1) % waypoints.Length;
+                    navMA.SetDestination(waypoints[currentWaypoint].position);
+                }
+                break;
+            case GuardState.Pursue:
+                break;
+            default:
+                break;
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            
+            playerTarget = other.gameObject;
+            SwitchToState(GuardState.Pursue);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            playerTarget = other.gameObject;
+            SwitchToState(GuardState.Patrol);
+        }
+
+    }
+}
+
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+
+    public bool Hover = false; 
+    
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    //public bool hover = false;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        }
+
+        
+
+        if (Input.GetButtonDown("fly"))
+        {
+            Hover = !Hover;
+        }
+
+        if (Hover == false)
+        {
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+    }
+}
